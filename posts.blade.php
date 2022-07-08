06669a4aeb184255591ffddff2c83babe7724e7b
Ahmed Essam <85046148+AhmedEssam262@users.noreply.github.com>
2022-07-08 05:12:06 +0200

    Create posts.blade.php

---
+++ posts.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="app.css">
+    {{-- <script src="app.js"></script> --}}
+    <title>Home Page</title>
+</head>
+<body>
+
+    <article>
+        <h1><a href="/">Welcome to our page</a></h1>
+        <p> Hi,Iam Ahmed Essam and iam happy that you are here for more information please contact me and i wil reply as soon as possible, don't hesitste to call me</p>
+    </article>
+
+    <article>
+        <h1><a href="first-post">First Post</a></h1>
+
+        <p> Hi,Iam Ahmed Essam and iam happy that you are here for more information please contact me and i wil reply as soon as possible, don't hesitste to call me</p>
+    </article>
+
+    <article>
+        <h1><a href="second-post">Second Post</a></h1>
+
+        <p> Hi,Iam Ahmed Essam and iam happy that you are here for more information please contact me and i wil reply as soon as possible, don't hesitste to call me</p>
+    </article>
+
+    <article>
+        <h1><a href="third-post">Third Post</a></h1>
+
+        <p> Hi,Iam Ahmed Essam and iam happy that you are here for more information please contact me and i wil reply as soon as possible, don't hesitste to call me</p>
+    </article>
+</body>
+</html>
