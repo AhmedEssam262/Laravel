70b2a6d9b42af7b6d616fbbb9a142c0197191bcb
Ahmed Essam <85046148+AhmedEssam262@users.noreply.github.com>
2022-07-24 05:10:12 +0200

    Create posts.blade.php

---
+++ Query Builder/posts.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Posts</title>
+</head>
+<body>
+    <h1>Welcome to our posts</h1>
+<p>to create a post <a href='posts/create'>click here</a></p>
+<p>to show a post <a href='posts/show'>click here</a></p>
+</body>
+</html>
