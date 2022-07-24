660c124cda7b9051c51ae026494b1a4575a94932
Ahmed Essam <85046148+AhmedEssam262@users.noreply.github.com>
2022-07-24 05:08:07 +0200

    Update postsController.php

--- Query Builder/postsController.php
+++ Query Builder/postsController.php
@@ -7,26 +7,56 @@ use Illuminate\Support\Facades\DB;
 
 class postController extends Controller
 {
-    public function posts(){
+    public function posts()
+    {
         return view('posts');
     }
 
-    public function create(){
+    public function create()
+    {
 
         return view('postss.create');
     }
-    
-    public function show(){
+
+    public function show()
+    {
         $mydata = DB::table('posts')->get();
-        return view('postss.show',compact('mydata'));
+        return view('postss.show', compact('mydata'));
     }
 
 
-    public function insert(Request $request ){
+    public function insert(Request $request)
+    {
         DB::table('posts')->insert([
-            'title'=>$request->title,
-                'body'=>$request->body
+            'title' => $request->title,
+            'body' => $request->body
         ]);
         return response("Your data are added successfully");
     }
+    public function edit($id)
+    {
+        $myd =DB::table('posts')->get()->where('id',$id)->first();
+        return view('postss.edit',compact('myd'));
+    }
+    public function update(Request $request,$id)
+    {
+        DB::table('posts')->where('id',$id)->update([
+            'title'=>$request->title,
+            'body'=>$request->body
+        ]);
+        return redirect('posts/show');
+    }
+    public function delete($id)
+    {
+        DB::table('posts')->where('id',$id)->delete();
+        return redirect('posts/show');
+    }
+    public function deleteAll()
+    {
+        DB::table('posts')->truncate();
+        return redirect('posts/show');
+    }
+
+
+}
 
