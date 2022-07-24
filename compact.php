7755c2c803d91a490ed80bf1dff11f28b4450284
Ahmed Essam <85046148+AhmedEssam262@users.noreply.github.com>
2022-07-24 05:07:00 +0200

    Update compact.php

--- compact.php
+++ compact.php
@@ -1,6 +1,8 @@
 <!-- to pass variable to another view -->
+<?
     public function show()
     {
         $mydata = DB::table('posts')->get();
         return view('postss.show', compact('mydata'));
     }
+?>
